70b52ac9aa65d98c9f7908e32742d13275876bef
Anton Tetov <anton@tetov.se>
2021-04-22 15:25:14 +0200

    Deleting references from old site

--- old/components-next/post-body.js
+++
@@ -1,9 +0,0 @@
-import MarkdownContent from "./markdown-content";
-
-export default function PostBody({ markdown }) {
-  return (
-    <div className="max-w-2xl mx-auto">
-      <MarkdownContent markdown={markdown} />
-    </div>
-  );
-}
