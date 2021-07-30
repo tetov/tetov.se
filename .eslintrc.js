49d6ae42cf70f94b741f8e8d38296b85e6fe86d6
Anton Tetov <anton@tetov.se>
2021-07-30 14:23:35 +0200

    build(devEnv): ESLint for lint and default prettier formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,47 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:prettier/recommended",
-      "prettier"
-    ],
-    settings: {
-      react: {
-        version: "detect"
-      }
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
     },
-    env: {
-      browser: true,
-      node: true,
-      es6: true
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ["@typescript-eslint", "react", "graphql"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      ecmaVersion: 2018,
+      sourceType: "module",
     },
-    plugins: [
-      "@typescript-eslint",
-      "react",
-      "graphql"
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-        ecmaVersion: 2018,
-        sourceType: "module"
-      },
-      rules: {
-        "react/prop-types": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "graphql/template-strings": [
-          error,
-          {
-            env: "relay",
-            tagName: "graphql",
-            schemaJsonFilepath: "./src/__generated__/gatsby-introspection.json"
-          }
-        ]
-      },
-      overrides: [
+    rules: {
+      "react/prop-types": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "graphql/template-strings": [
+        error,
         {
-          files: [
-            "*.js"
-          ],
-          rules: {
-            "@typescript-eslint/no-var-requires": "off"
-          }
-        }
-      ]
-    }
-  }
+          env: "relay",
+          tagName: "graphql",
+          schemaJsonFilepath: "./src/__generated__/gatsby-introspection.json",
+        },
+      ],
+    },
+    overrides: [
+      {
+        files: ["*.js"],
+        rules: {
+          "@typescript-eslint/no-var-requires": "off",
+        },
+      },
+    ],
+  },
+};
