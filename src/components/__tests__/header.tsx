1d311e6b6b46255f76722f72ff9f39abc71f34cf
Anton Tetov <anton@tetov.se>
2022-10-11 00:06:22 +0200

    clean tests and update header test

--- src/components/__tests__/header.tsx
+++ src/components/__tests__/header.tsx
@@ -1,13 +1,24 @@
+import * as Gatsby from "gatsby";
 import React from "react";
 import renderer from "react-test-renderer";
-import { PureHeader as Header } from "src/components/header";
+import Header from "src/components/header";
 
 describe("Header", () => {
   it("renders correctly", () => {
+    const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery");
+    useStaticQuery.mockImplementation(() => ({
+      site: {
+        siteMetadata: {
+          title: `Nossnahoj Votet Notna`,
+          description: `Mocks`,
+          siteURL: `https://example.ai`,
+          lang: `ent`,
+          image: `/logo.png`,
+        },
+      },
+    }));
     const tree = renderer
-      .create(
-        <Header title="tetov's projects and more" subHeading={<div></div>} />
-      )
+      .create(<Header subHeading={<span>Test</span>} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
