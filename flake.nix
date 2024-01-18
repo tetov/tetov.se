001bb8be6110a5b0423748b80ef427a6a11ea763
Anton Tetov <anton@tetov.se>
2024-01-18 19:28:01 +0100

    nix shell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Shell env for blog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+        devShells.default = pkgs.mkShell {
+          buildInputs = [pkgs.nodejs_20];
+        };
+        formatter = pkgs.alejandra;
+      };
+      flake = {};
+    };
+}
